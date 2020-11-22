ae6e07129152a901fea2973a1c4421b74d37eb0c
Emil Oja <emil.oja@gmail.com>
2020-11-22 09:52:51 +0100

    chore: fixed ui feedback on collect

--- src/app/services/collection/collection.service.ts
+++ src/app/services/collection/collection.service.ts
@@ -13,17 +13,13 @@ export class CollectionService {
     if (localStorage.getItem('pokemons') == null) {
       let temp = [];
       temp.push(pokemon)
-      alert("Pokemons collected: " + pokemon);
+      
       localStorage.setItem('pokemons', JSON.stringify(temp));
     } else {
       let temp = [];
       temp = JSON.parse(localStorage.getItem('pokemons')) || [];
-      if (temp.includes(pokemon)) {
-        alert("ERROR: Pokemon allready collected")
-      } else {
-        temp.push(pokemon);
-        alert("Pokemons collected: " + pokemon);
-      }
+      temp.push(pokemon);
+
       localStorage.setItem('pokemons', JSON.stringify(temp));
     }
   }
